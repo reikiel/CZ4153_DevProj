aa10b75794437aa6af1fb53d792f189169d0b82b
Valerie Tan <valerietan7@gmail.com>
2021-10-12 00:08:00 +0800

    transfer tokens to relevant companies

--- contracts/DGT.sol
+++ contracts/DGT.sol
@@ -2,7 +2,12 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DGT is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Daimler Governance Token", "DGT") public {
-        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    constructor(uint256 initialSupply, address[] memory _addr) ERC20("Daimler Governance Token", "DGT") public {
+        _mint(msg.sender, initialSupply * (10 ** decimals())); // Company A
+        transfer(_addr[1], 2000000 * (10 ** decimals())); // Company B
+        transfer(_addr[2], 2000000 * (10 ** decimals())); // Company C
+        transfer(_addr[3], 1000000 * (10 ** decimals())); // Company D
+        transfer(_addr[4], 1000000 * (10 ** decimals())); // Platform User #1
+        transfer(_addr[5], 1000000 * (10 ** decimals())); // Platform User #2
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var DGT = artifacts.require("./DGT.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(DGT, 10000000);
+module.exports = function (deployer, network, accounts) {
+    deployer.deploy(DGT, 10000000, accounts);
 };
