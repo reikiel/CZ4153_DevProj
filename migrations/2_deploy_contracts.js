6924731c96ccec3532efec54376487c52f9fa1ac
Valerie Tan <valerietan7@gmail.com>
2021-11-18 22:25:44 +0800

    minting logic

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,12 @@ var DGT = artifacts.require("./DGT.sol");
 var Accounts = artifacts.require("./Accounts.sol");
 var Pool = artifacts.require("./Pool.sol");
 var Ideas = artifacts.require("./Ideas.sol");
-var IdeaTest = artifacts.require("./IdeaTest.sol");
 
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(DGT, 10000000, accounts);
     await deployer.deploy(Pool);
     await deployer.deploy(Ideas);
     await deployer.deploy(Accounts, accounts);
-    await deployer.deploy(IdeaTest);
 
     // set dgt ABI so that pool can use its functions
     let dgt = await DGT.deployed();
@@ -17,6 +15,7 @@ module.exports = async function (deployer, network, accounts) {
     let accountsContract = await Accounts.deployed();
     let ideas = await Ideas.deployed();
     await pool.setDgtAddress(dgt.address);
+    await pool.setAccountsAddress(accountsContract.address);
     await ideas.setDgtAddress(dgt.address);
     await ideas.setPoolAddress(pool.address);
     await ideas.setAccountsAddress(accountsContract.address);
